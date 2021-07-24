a9ce0a9480ca3f60c0e0cf7fd69ec180c8e8082f
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-07-24 14:10:46 +0300

    portfolio display project name using jquery done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,110 @@
 $(document).ready(function () {
+
+    //  what we do section
+
+
+
+
+
+    
+    // portfolio effects
+    $(".div1").hover(function () {
+        // over
+        $(".po1").animate({ opacity: 1 }, 50);
+        $(".pn1").addClass("portfolio-hover-effect");
+    }
+        , function () {
+            // out
+            $(".po1").animate({ opacity: 0 }, 50);
+            $(".pn1").removeClass("portfolio-hover-effect");
+        }
+    );
+    
+    $(".div2").hover(function () {
+        // over
+        $(".po2").animate({ opacity: 1 }, 50);
+        $(".pn2").addClass("portfolio-hover-effect");
+    }
+        , function () {
+            // out
+            $(".po2").animate({ opacity: 0 }, 50);
+            $(".pn2").removeClass("portfolio-hover-effect");
+        }
+    );
+
+    $(".div3").hover(function () {
+        // over
+        $(".po3").animate({ opacity: 1 }, 50);
+        $(".pn3").addClass("portfolio-hover-effect");
+    }
+        , function () {
+            // out
+            $(".po3").animate({ opacity: 0 }, 50);
+            $(".pn3").removeClass("portfolio-hover-effect");
+        }
+    );
+
+    $(".div4").hover(function () {
+        // over
+        $(".po4").animate({ opacity: 1 }, 50);
+        $(".pn4").addClass("portfolio-hover-effect");
+    }
+        , function () {
+            // out
+            $(".po4").animate({ opacity: 0 }, 50);
+            $(".pn4").removeClass("portfolio-hover-effect");
+        }
+    );
+
+    $(".div5").hover(function () {
+        // over
+        $(".po5").animate({ opacity: 1 }, 50);
+        $(".pn5").addClass("portfolio-hover-effect");
+    }
+        , function () {
+            // out
+            $(".po5").animate({ opacity: 0 }, 50);
+            $(".pn5").removeClass("portfolio-hover-effect");
+        }
+    );
+
+    $(".div6").hover(function () {
+        // over
+        $(".po6").animate({ opacity: 1 }, 50);
+        $(".pn6").addClass("portfolio-hover-effect");
+    }
+        , function () {
+            // out
+            $(".po6").animate({ opacity: 0 }, 50);
+            $(".pn6").removeClass("portfolio-hover-effect");
+        }
+    );
+
+    $(".div7").hover(function () {
+        // over
+        $(".po7").animate({ opacity: 1 }, 50);
+        $(".pn7").addClass("portfolio-hover-effect");
+    }
+        , function () {
+            // out
+            $(".po7").animate({ opacity: 0 }, 50);
+            $(".pn7").removeClass("portfolio-hover-effect");
+        }
+    );
+
+    $(".div8").hover(function () {
+        // over
+        $(".po8").animate({ opacity: 1 }, 50);
+        $(".pn8").addClass("portfolio-hover-effect");
+    }
+        , function () {
+            // out
+            $(".po8").animate({ opacity: 0 }, 50);
+            $(".pn8").removeClass("portfolio-hover-effect");
+        }
+    );
+
+    // contact us
     $("#contact-form form").submit(function (e) {
         e.preventDefault();
         var name = $("#name").val();
@@ -32,7 +138,7 @@ $(document).ready(function () {
             $("#name-error").html("");
         }
         // add mailchimp
-        
+
         // integrate MailChimp
         // var data = {
         //     "name": name,
